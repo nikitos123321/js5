046b7354d414968c738c9c463be3b2266351a00b
nikitos123321 <nikitasaronov1907@gmail.com>
2024-09-14 08:26:51 +0300

    Практика 6

--- main.js
+++ main.js
@@ -2,91 +2,44 @@
 
 // 1
 
-const salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-};
+let div = document.body.children[0];
+let ul = document.body.children[1];
+let pit = document.body.children[1].lastElementChild;
+console.log(div, ul, pit)
 
-let sum = salaries.John + salaries.Ann + salaries.Pete;
-
-console.log(sum);
 
 // 2
 
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-};
-function multiplyNumeric() {
-    menu.width  *= 2;
-    menu.height *=2;
+const listItems = document.querySelectorAll("li");
 
+for (const item of listItems) {
+  item.classList.add("list-item");
 }
-multiplyNumeric();
-console.log(menu);
 
 
 // 3
 
-const calculator = {
-   
-    read() {
-      this.a = +prompt("Введите первое число:");
-      this.b = +prompt("Введите второе число:");
-    },
-  
-  
-    sum() {
-      return this.a + this.b;
-    },
-  
-  
-    mul() {
-      return this.a * this.b;
-    }
-  };
+let num = +prompt("Введите количество блоков");
 
-calculator.read();
-alert(calculator.sum());
-alert(calculator.mul()) ;
-
-
-//  4
-
-let str = "$120";
-
-function extractCurrencyValue(str) {
-    return +str.slice(1);
+for (let i = 0; i < num; i++){
+  let div = document.createElement('div');
+  document.body.children[1].after(div)
 }
-console.log(extractCurrencyValue(str));
-  
-
-// 5
-
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
-
-let users = [ vasya, petya, masha ];
-
-let names = users.map(user => user.name);
-
-alert( names ); 
-
- 
-// 6
 
 
-function getAverageAge(users) {
-    let totalAge = 0;
-  
-    for (const user of users) {
-      totalAge += user.age;
-    }
-  let kk =  totalAge / users.length
-    alert(kk)
-  }
+// 4
 
-  getAverageAge(users)
+let form = document.createElement('form');
+form.className = "form";
+let name = document.createElement('input');
+name.placeholder = "Имя"
+let email = document.createElement('input');
+email.placeholder = "Почта";
+let submit = document.createElement('input');
+submit.value = "Отправить";
+submit.type = "submit"
+
+document.getElementById('form').prepend(form)
+document.querySelector('form').append(name);
+document.querySelector('form').append(email);
+document.querySelector('form').append(submit);
